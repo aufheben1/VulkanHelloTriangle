3873731412a8aa18252e1c67c629c70f206e1c49
heben_lab <aufheben_1@naver.com>
2017-03-27 18:00:01 +0900

    Add 'createInstance'

--- VulkanHelloTriangle/VulkanHelloTriangle/HelloTriangleApplication.h
+++ VulkanHelloTriangle/VulkanHelloTriangle/HelloTriangleApplication.h
@@ -1,13 +1,51 @@
 #ifndef _VULKAN_HELLO_TRIANGLE_APPLICATION_H_
 #define _VULKAN_HELLO_TRIANGLE_APPLICATION_H_
 
+#include <glm/glm.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+#include <vulkan/vulkan.hpp>
+
+#include <iostream>
+#include <vector>
+
 class HelloTriangleApplication {
 public:
 	void run();
+	void terminate();
 
 private:
 	void initVulkan();
 	void mainLoop();
+	
+	void createInstance();
+	void selectPhysicalDevice();
+	bool isDeviceSuitable(vk::PhysicalDevice device);
+	void createLogicalDevice();
+	
+	
+	
+	void destroyInstance();
+
+public:
+
+private:
+	vk::Instance instance = VK_NULL_HANDLE;
+	std::vector<const char*> extensions;
+	std::vector<const char*> layers;
+
+	vk::ApplicationInfo appInfo;
+	vk::InstanceCreateInfo instInfo;
+
+	vk::PhysicalDevice gpu = VK_NULL_HANDLE;
+	vk::PhysicalDeviceProperties gpuProps = {};
+	vk::PhysicalDeviceMemoryProperties gpuMemoryProps = {};
+
+	vk::SurfaceKHR surface;
+	SDL_Window* window;
+
+	vk::SurfaceKHR createVulkanSurface(const vk::Instance& instance, SDL_Window* window);
+	std::vector<const char*> getAvailableWSIExtensions();
 };
 
 
--- VulkanHelloTriangle/VulkanHelloTriangle/main.cpp
+++ VulkanHelloTriangle/VulkanHelloTriangle/main.cpp
@@ -59,41 +59,8 @@ int main()
 		return EXIT_FAILURE;
 	}
 
+	app.terminate();
 	return EXIT_SUCCESS;
-    // Use validation layers if this is a debug build, and use WSI extensions regardless
-    std::vector<const char*> extensions = getAvailableWSIExtensions();
-    std::vector<const char*> layers;
-#if defined(_DEBUG)
-    layers.push_back("VK_LAYER_LUNARG_standard_validation");
-#endif
-
-    // vk::ApplicationInfo allows the programmer to specifiy some basic information about the
-    // program, which can be useful for layers and tools to provide more debug information.
-    vk::ApplicationInfo appInfo = vk::ApplicationInfo()
-        .setPApplicationName("Vulkan C++ Windowed Program Template")
-        .setApplicationVersion(1)
-        .setPEngineName("LunarG SDK")
-        .setEngineVersion(1)
-        .setApiVersion(VK_API_VERSION_1_0);
-
-    // vk::InstanceCreateInfo is where the programmer specifies the layers and/or extensions that
-    // are needed.
-    vk::InstanceCreateInfo instInfo = vk::InstanceCreateInfo()
-        .setFlags(vk::InstanceCreateFlags())
-        .setPApplicationInfo(&appInfo)
-        .setEnabledExtensionCount(static_cast<uint32_t>(extensions.size()))
-        .setPpEnabledExtensionNames(extensions.data())
-        .setEnabledLayerCount(static_cast<uint32_t>(layers.size()))
-        .setPpEnabledLayerNames(layers.data());
-
-    // Create the Vulkan instance.
-    vk::Instance instance;
-    try {
-        instance = vk::createInstance(instInfo);
-    } catch(const std::exception& e) {
-        std::cout << "Could not create a Vulkan instance: " << e.what() << std::endl;
-        return 1;
-    }
 
     // Create an SDL window that supports Vulkan and OpenGL rendering.
     if(SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -148,87 +115,4 @@ int main()
     instance.destroy();
 
     return 0;
-}
-
-vk::SurfaceKHR createVulkanSurface(const vk::Instance& instance, SDL_Window* window)
-{
-    SDL_SysWMinfo windowInfo;
-    SDL_VERSION(&windowInfo.version);
-    if(!SDL_GetWindowWMInfo(window, &windowInfo)) {
-        throw std::system_error(std::error_code(), "SDK window manager info is not available.");
-    }
-
-    switch(windowInfo.subsystem) {
-
-#if defined(SDL_VIDEO_DRIVER_ANDROID) && defined(VK_USE_PLATFORM_ANDROID_KHR)
-    case SDL_SYSWM_ANDROID: {
-        vk::AndroidSurfaceCreateInfoKHR surfaceInfo = vk::AndroidSurfaceCreateInfoKHR()
-            .setWindow(windowInfo.info.android.window);
-        return instance.createAndroidSurfaceKHR(surfaceInfo);
-    }
-#endif
-
-#if defined(SDL_VIDEO_DRIVER_MIR) && defined(VK_USE_PLATFORM_MIR_KHR)
-    case SDL_SYSWM_MIR: {
-        vk::MirSurfaceCreateInfoKHR surfaceInfo = vk::MirSurfaceCreateInfoKHR()
-            .setConnection(windowInfo.info.mir.connection)
-            .setMirSurface(windowInfo.info.mir.surface);
-        return instance.createMirSurfaceKHR(surfaceInfo);
-    }
-#endif
-
-#if defined(SDL_VIDEO_DRIVER_WAYLAND) && defined(VK_USE_PLATFORM_WAYLAND_KHR)
-    case SDL_SYSWM_WAYLAND: {
-        vk::WaylandSurfaceCreateInfoKHR surfaceInfo = vk::WaylandSurfaceCreateInfoKHR()
-            .setDisplay(windowInfo.info.wl.display)
-            .setSurface(windowInfo.info.wl.surface);
-        return instance.createWaylandSurfaceKHR(surfaceInfo);
-    }
-#endif
-
-#if defined(SDL_VIDEO_DRIVER_WINDOWS) && defined(VK_USE_PLATFORM_WIN32_KHR)
-    case SDL_SYSWM_WINDOWS: {
-        vk::Win32SurfaceCreateInfoKHR surfaceInfo = vk::Win32SurfaceCreateInfoKHR()
-            .setHinstance(GetModuleHandle(NULL))
-            .setHwnd(windowInfo.info.win.window);
-        return instance.createWin32SurfaceKHR(surfaceInfo);
-    }
-#endif
-
-#if defined(SDL_VIDEO_DRIVER_X11) && defined(VK_USE_PLATFORM_XLIB_KHR)
-    case SDL_SYSWM_X11: {
-        vk::XlibSurfaceCreateInfoKHR surfaceInfo = vk::XlibSurfaceCreateInfoKHR()
-            .setDpy(windowInfo.info.x11.display)
-            .setWindow(windowInfo.info.x11.window);
-        return instance.createXlibSurfaceKHR(surfaceInfo);
-    }
-#endif
-
-    default:
-        throw std::system_error(std::error_code(), "Unsupported window manager is in use.");
-    }
-}
-
-std::vector<const char*> getAvailableWSIExtensions()
-{
-    std::vector<const char*> extensions;
-    extensions.push_back(VK_KHR_SURFACE_EXTENSION_NAME);
-
-#if defined(VK_USE_PLATFORM_ANDROID_KHR)
-    extensions.push_back(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
-#endif
-#if defined(VK_USE_PLATFORM_MIR_KHR)
-    extensions.push_back(VK_KHR_MIR_SURFACE_EXTENSION_NAME);
-#endif
-#if defined(VK_USE_PLATFORM_WAYLAND_KHR)
-    extensions.push_back(VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME);
-#endif
-#if defined(VK_USE_PLATFORM_WIN32_KHR)
-    extensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
-#endif
-#if defined(VK_USE_PLATFORM_XLIB_KHR)
-    extensions.push_back(VK_KHR_XLIB_SURFACE_EXTENSION_NAME);
-#endif
-
-    return extensions;
 }
